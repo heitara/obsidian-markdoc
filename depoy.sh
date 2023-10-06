47bd229f5939f245cd6364878c326376a0611a95
Emil Atanasov <heitara@gmail.com>
2023-10-06 13:44:45 +0300

    fix: add missing plugins folder

--- depoy.sh
+++ depoy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TO_VAULT=$1
+PLUGINS=plugins
 OBSIDIAN=.obsidian
 SUBFOLDER="mdoc"
 # check if .obsidian folder exists
@@ -9,15 +10,18 @@ else
   echo "The folder $1 does contian $OBSIDIAN subfolder!"
   exit 1
 fi
+# create plugins
+if [ ! -d "$1/$OBSIDIAN/$PLUGINS" ]; then
+  mkdir -p "$1/$OBSIDIAN/$PLUGINS"
+fi
 
 # create mdoc folder
 
-if [ ! -d "$1/$OBSIDIAN/$SUBFOLDER" ]; then
-  mkdir -p "$1/$OBSIDIAN/$SUBFOLDER"
-  echo "Subfolder '$subfolder' created."
+if [ ! -d "$1/$OBSIDIAN/$PLUGINS/$SUBFOLDER" ]; then
+  mkdir -p "$1/$OBSIDIAN/$PLUGINS/$SUBFOLDER"
 fi
 
 # copy files
-cp manifest.json "$1/$OBSIDIAN/$SUBFOLDER"
-cp main.js "$1/$OBSIDIAN/$SUBFOLDER"
-cp styles.css "$1/$OBSIDIAN/$SUBFOLDER"
+cp manifest.json "$1/$OBSIDIAN/$PLUGINS/$SUBFOLDER"
+cp main.js "$1/$OBSIDIAN/$PLUGINS/$SUBFOLDER"
+cp styles.css "$1/$OBSIDIAN/$PLUGINS/$SUBFOLDER"
